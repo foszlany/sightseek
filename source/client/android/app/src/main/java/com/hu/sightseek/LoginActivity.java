c4c09a747534e1b51d81e9a681816aa4ee93c059
foszlany <radojcickilienborisz@gmail.com>
2025-09-02 23:13:39 +0200

    Register btn on login page, password fix

--- source/client/android/app/src/main/java/com/hu/sightseek/LoginActivity.java
+++ source/client/android/app/src/main/java/com/hu/sightseek/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.hu.sightseek;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,34 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.osmdroid.config.Configuration;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Configuration.getInstance().load(
+                getApplicationContext(),
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext())
+        );
+        Configuration.getInstance().setUserAgentValue(getPackageName());
+
+        // Login button
+        Button loginButton = findViewById(R.id.login_loginbtn);
+        loginButton.setOnClickListener(view -> {
+            // Check stuff...
+
+            // Intent intent = new Intent(this, MainActivity.class);
+            // startActivity(intent);
+        });
+
+        // Register button
+        Button registerButton = findViewById(R.id.login_registerbtn);
+        registerButton.setOnClickListener(view -> {
+            Intent intent = new Intent(this, RegisterActivity.class);
+            startActivity(intent);
         });
     }
 }
