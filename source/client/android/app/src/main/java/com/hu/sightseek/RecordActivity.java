45b797ace59555c0b375b0d482094c14b0cf2d7a
foszlany <radojcickilienborisz@gmail.com>
2025-09-02 23:08:18 +0200

    Random

--- source/client/android/app/src/main/java/com/hu/sightseek/RecordActivity.java
+++ source/client/android/app/src/main/java/com/hu/sightseek/RecordActivity.java
@@ -104,7 +104,7 @@ public class RecordActivity extends AppCompatActivity {
         // TODO: MOVE THIS TO MAIN ACTIVITY!!!
         // Show banner when launching for the first time
         SharedPreferences prefs = getSharedPreferences("AppPrefs", MODE_PRIVATE);
-        boolean isFirstLaunch = prefs.getBoolean("isFirstLauncha", true);
+        boolean isFirstLaunch = prefs.getBoolean("isFirstLauncha", true); // TODO: Remove 'a' once done testing
 
         if(isFirstLaunch) {
             startActivity(new Intent(this, BannerActivity.class));
