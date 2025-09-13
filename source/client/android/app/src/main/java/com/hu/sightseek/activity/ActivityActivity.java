f82da6f287f56da17cc83ddd2407c96906a743ff
foszlany <radojcickilienborisz@gmail.com>
2025-09-13 14:40:24 +0200

    Base ActivityActivity

---
+++ source/client/android/app/src/main/java/com/hu/sightseek/activity/ActivityActivity.java
@@ -0,0 +1,74 @@
+package com.hu.sightseek.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.hu.sightseek.R;
+
+import org.osmdroid.config.Configuration;
+
+public class ActivityActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity);
+        Configuration.getInstance().load(
+                getApplicationContext(),
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext())
+        );
+        Configuration.getInstance().setUserAgentValue(getPackageName());
+
+        // Add Menu
+        Toolbar toolbar = findViewById(R.id.menubar_activity);
+        setSupportActionBar(toolbar);
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+
+        // Home button
+        toolbar.setNavigationIcon(R.drawable.baseline_home_24);
+        toolbar.setNavigationOnClickListener(v -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    // Create top menubar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_top, menu);
+        return true;
+    }
+
+    // Top menubar actions
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        // Profile
+        if(id == R.id.topmenu_profile) {
+            // TODO: Check whether user is logged in
+            Intent intent = new Intent(this, BannerActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        // Statistics
+        if(id == R.id.topmenu_statistics) {
+            // TODO
+            // Intent intent = new Intent(this, StatisticsActivity.class);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
