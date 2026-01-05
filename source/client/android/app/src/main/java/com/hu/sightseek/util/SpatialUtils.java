bbdceb22393ad103a70ae0ec5a6aa0b1822857fe
foszlany <radojcickilienborisz@gmail.com>
2026-01-05 10:50:26 +0100

    Remove unused encode method

--- source/client/android/app/src/main/java/com/hu/sightseek/util/SpatialUtils.java
+++ source/client/android/app/src/main/java/com/hu/sightseek/util/SpatialUtils.java
@@ -56,36 +56,7 @@ public class SpatialUtils {
         return new BoundingBox(maxLat, maxLon, minLat, minLon);
     }
 
-    // From Google Maps API Library's PolyUtil class rewritten for OSMDroid's GeoPoint
-    public static String encode(final List<GeoPoint> path) {
-        long lastLat = 0;
-        long lastLng = 0;
-
-        final StringBuffer result = new StringBuffer();
-
-        for(final GeoPoint point : path) {
-            long lat = Math.round(point.getLatitude() * 1e5);
-            long lng = Math.round(point.getLongitude() * 1e5);
-
-            long dLat = lat - lastLat;
-            long dLng = lng - lastLng;
-
-            encode(dLat, result);
-            encode(dLng, result);
-
-            lastLat = lat;
-            lastLng = lng;
-        }
-        return result.toString();
-    }
-    private static void encode(long v, StringBuffer result) {
-        v = v < 0 ? ~(v << 1) : v << 1;
-        while(v >= 0x20) {
-            result.append(Character.toChars((int) ((0x20 | (v & 0x1f)) + 63)));
-            v >>= 5;
-        }
-        result.append(Character.toChars((int) (v + 63)));
-    }
+    // From Google Maps API Library's PolyUtil class modified for OSMDroid's GeoPoint
     public static List<GeoPoint> decode(final String encodedPath) {
         int len = encodedPath.length();
 
