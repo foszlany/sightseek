4f0ce4f0ba35b7d9bf421038d9a5fc14cf4267fb
foszlany <radojcickilienborisz@gmail.com>
2026-01-11 00:16:47 +0100

    Documentation for IdeaGeoPoint

--- source/client/android/app/src/main/java/com/hu/sightseek/model/IdeaGeoPoint.java
+++ source/client/android/app/src/main/java/com/hu/sightseek/model/IdeaGeoPoint.java
@@ -2,9 +2,18 @@ package com.hu.sightseek.model;
 
 import org.osmdroid.views.overlay.simplefastpoint.LabelledGeoPoint;
 
+/** A GeoPoint representing an Idea (Attraction). */
 public class IdeaGeoPoint extends LabelledGeoPoint {
+    /** ID of the Idea */
     private final long id;
 
+    /**
+     * Constructor
+     * @param aLatitude Latitude
+     * @param aLongitude Longitude
+     * @param aLabel Name of the Idea
+     * @param id ID of the Idea
+     */
     public IdeaGeoPoint(double aLatitude, double aLongitude, String aLabel, long id) {
         super(aLatitude, aLongitude, aLabel);
 
